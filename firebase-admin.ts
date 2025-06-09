599fc9e8316f25fa80048a4c2f1ee80b5275db39
Ayush-8955 <ayush058955@gmail.com>
2025-06-10 01:37:16 +0530

    updation in firebase-admin file to fix  importing module service_key.json

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -3,16 +3,27 @@
 import {initializeApp, getApps, App, getApp, cert,  } from "firebase-admin/app"
 import {getFirestore} from "firebase-admin/firestore"
 
-const serviceKey = require("@/service_key.json")
+// const serviceKey = require("@/service_key.json")
 
 // import serviceKey from "@/service_key.json" assert { type: "json" };
 
 
 let app:App;   // define type of app
 
+// Pull the JSON string from the environment
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+if (!serviceAccountString) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY env var is missing");
+}
+
+// Parse it back into an object
+const serviceAccount = JSON.parse(serviceAccountString);
+
+
+
 if(getApps().length===0){
     app=initializeApp({
-        credential:cert(serviceKey)
+        credential:cert(serviceAccount)
     });
 }else{
     app=getApp();
